19a56c1f1c1a085921ece241c994b6e2be5fb1f7
ManzDev <JoseRomanHernandez@gmail.com>
2026-04-19 20:40:49 +0100

    Cleanup: remove old file

--- src/modules/message/emotes/createFragments-copy.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line
-export const createFragments = (message: string) => {
-  const parts = message.split(/(@[a-zA-Z0-9_]+)/g).filter(Boolean);
-
-  return parts.map(part => ({
-    type: /^@[a-zA-Z0-9_]+$/.test(part) ? "nick" : "text",
-    data: part
-  }));
-};
