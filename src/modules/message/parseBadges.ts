04b2cdaedfcdf712590a03d7293d0c71e173f830
ManzDev <JoseRomanHernandez@gmail.com>
2026-04-19 21:14:24 +0100

    Update some badges and default unknown badge

--- src/modules/message/parseBadges.ts
+++ src/modules/message/parseBadges.ts
@@ -3,6 +3,7 @@ import { BadgeInfoType, parseBadgeInfo } from "./badges/parseBadgeInfo";
 import badgesInfo from "./badges/badges";
 
 const MAX_SUB_IMAGE = "https://static-cdn.jtvnw.net/badges/v1/ed51a614-2c44-4a60-80b6-62908436b43a/3";
+const UNKNOWN_BADGE = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNzIiIGhlaWdodD0iNzIiIHZpZXdCb3g9IjAgMCAxOSAxOSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCBmaWxsPSIjNzc3IiB4PSIwIiB5PSIwIiB3aWR0aD0iMTkiIGhlaWdodD0iMTkiIHJ4PSIzIiAvPjxjaXJjbGUgc3Ryb2tlPSIjZWVlIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiIGN4PSI5LjUiIGN5PSI5LjUiIHI9IjMiIC8+PC9zdmc+";
 
 export const parseBadges = (fields : any) : Array<BadgeInfoType> => {
   const badges : any = parseSlashToString(fields.badges) || {};
@@ -30,7 +31,12 @@ export const parseBadges = (fields : any) : Array<BadgeInfoType> => {
       description: keyData.description
     };
 
-    name === "subscriber" && data.value > 6 && !data.image && (data.image = MAX_SUB_IMAGE);
+    if (!data.image) {
+      if ((data.name === "subscriber") && (Number(data.value) > 6))
+        data.image = MAX_SUB_IMAGE;
+      else
+        data.image = UNKNOWN_BADGE;
+    }
     name === "subscriber" && (data.fullMonths = Number(badgeInfo.subscriber));
     name === "founder" && (data.founderNumber = Number(badgeInfo.founder));
     name === "predictions" && (data.predictionInfo = cleanMessage(badgeInfo.predictions));
